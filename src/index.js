09b0db4f91d47d64637560f0d6349cdb7ddd965b
Volodymyr Savchuk <savchuk.v03@gmail.com>
2023-09-06 17:43:56 +0300

    add prop types

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+   const [movieRating, setMovieRating] = useState(0);
+   return (
+      <>
+         <StarRating color="blue" onSetRating={setMovieRating} />
+         <p>This movie was rated {movieRating} stars</p>
+      </>
+   );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
    <React.StrictMode>
-      <App />
+      <StarRating
+         maxRating={5}
+         messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+         defaultRating={2}
+      />
+      <Test />
    </React.StrictMode>
 );
