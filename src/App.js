ce359074ae43d72fe17bc7556f2ac4c19e70003c
Volodymyr Savchuk <savchuk.v03@gmail.com>
2024-01-03 13:30:24 +0200

    Implement focus on search feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import StarRating from "./StarRating";
 
 function average(arr) {
@@ -148,6 +148,26 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+   const inputElement = useRef(null);
+
+   useEffect(
+      function () {
+         function callback(e) {
+            if (document.activeElement === inputElement.current) return;
+
+            if (e.code === "Enter") {
+               inputElement.current.focus();
+               setQuery("");
+            }
+         }
+
+         document.addEventListener("keydown", callback);
+
+         return () => document.removeEventListener("keydown", callback);
+      },
+      [setQuery]
+   );
+
    return (
       <input
          className="search"
@@ -155,6 +175,7 @@ function Search({ query, setQuery }) {
          placeholder="Search movies..."
          value={query}
          onChange={(e) => setQuery(e.target.value)}
+         ref={inputElement}
       />
    );
 }
